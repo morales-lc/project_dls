eec45e24d5690f477e605ce6f9f54d97b8eab97a
morales-lc <thejeromeee@gmail.com>
2026-03-17 14:25:53 +0800

    proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,11 +3,25 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
     protected $proxies = '*';
 
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
 }
